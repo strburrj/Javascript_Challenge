246c63f868bbf74b3f040bf4fc38d092aab98a5b
strburrj <57110745+strburrj@users.noreply.github.com>
2020-04-08 12:22:17 -0500

    level 1 push

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var dateInput = d3.select("#datetime");
+var outputufotable = d3.select("#ufo-table");
+var button = d3.select("#filter-btn");
+button.on("click", function () {
+
+
+});
+
+outputufotable.select("tbody")
+    .selectAll("tr")
+    .data(tableData)
+    .enter()
+    .append("tr")
+    .html(function (d) {
+        return `<td>${d.datetime}</td>
+            <td>${d.city}</td>
+            <td>${d.state}</td>
+            <td>${d.country}</td>
+            <td>${d.shape}</td>
+            <td>${d.durationMinutes}</td>
+            <td>${d.comments}</td>`;
+    });
+
+
